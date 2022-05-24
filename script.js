bc3ed99dd8826dc8ae58ebc7a4bbb3ad327610e5
Chris McClain <chrismac539@gmail.com>
2022-05-24 01:40:48 -0400

    created nested loop inoder to create accurate # of grids, play around with css not sure why the height is off?

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
- function test (){
-  console.log('test')
+const container = document.querySelector('#container');
+
+// create grid div and grid class
+
+function createGrid(x) {
+  for (let rows = 0; rows < x; rows++) {
+    for (let columns = 0; columns < x; columns++) {
+      const grid = document.createElement('div');
+      grid.classList.add('grid');
+      container.appendChild(grid)
+    }
+  }
+
 }
 
-test ();
+createGrid(16);
